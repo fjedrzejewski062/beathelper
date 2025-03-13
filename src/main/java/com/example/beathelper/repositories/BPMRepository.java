6e406546fe4f97eb8187fa9beb497c033609cb26
Filip Jedrzejewski <fjedrzejewski062@gmail.com>
2025-03-13 15:28:58 +0100

    Poprawienie BPMRepository i KeyRepository

--- src/main/java/com/example/beathelper/repositories/BPMRepository.java
+++ src/main/java/com/example/beathelper/repositories/BPMRepository.java
@@ -1,10 +1,13 @@
 package com.example.beathelper.repositories;
 
 import com.example.beathelper.entities.BPM;
+import com.example.beathelper.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BPMRepository extends JpaRepository<BPM, Long> {
     Optional<BPM> findByBPM(Integer bpmValue);
+    List<BPM> findByUser(User user);
 }
--- src/main/java/com/example/beathelper/repositories/KeyRepository.java
+++ src/main/java/com/example/beathelper/repositories/KeyRepository.java
@@ -1,11 +1,14 @@
 package com.example.beathelper.repositories;
 
 import com.example.beathelper.entities.Key;
+import com.example.beathelper.entities.User;
 import com.example.beathelper.enums.KeyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface KeyRepository extends JpaRepository<Key, Long> {
     Optional<Key> findByKey(KeyType key);
+    List<Key> findByUser(User user);
 }
