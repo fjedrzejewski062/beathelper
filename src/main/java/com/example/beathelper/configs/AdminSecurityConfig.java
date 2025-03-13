90e246af0dbc47f21f1c95447076b70e4ef033e1
Filip Jedrzejewski <fjedrzejewski062@gmail.com>
2025-03-13 15:30:29 +0100

    Dodanie AdminSecurityConfig

---
+++ src/main/java/com/example/beathelper/configs/AdminSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.beathelper.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Order(1)
+public class AdminSecurityConfig {
+    @Bean
+    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .securityMatcher("/admin/**")
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/admin/login").permitAll()
+                        .anyRequest().hasAuthority("ADMIN")
+                )
+                .formLogin(form -> form
+                        .loginPage("/admin/login")
+                        .defaultSuccessUrl("/admin/dashboard", true)
+                        .loginProcessingUrl("/admin/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/admin/logout")
+                        .logoutSuccessUrl("/admin/login?logout")
+                        .permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
