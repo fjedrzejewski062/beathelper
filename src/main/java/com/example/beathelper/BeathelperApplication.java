bddbe1eb9a4be7846626e62a866ecda45bd0574b
Filip Jedrzejewski <fjedrzejewski062@gmail.com>
2025-03-27 17:21:56 +0100

    Poprawa editBPM

--- src/main/java/com/example/beathelper/BeathelperApplication.java
+++ src/main/java/com/example/beathelper/BeathelperApplication.java
@@ -2,11 +2,15 @@ package com.example.beathelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class BeathelperApplication {
 
 	public static void main(String[] args) {
+//		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//		String encodedPassword = encoder.encode("123456");
+//		System.out.println(encodedPassword);
 		SpringApplication.run(BeathelperApplication.class, args);
 	}
 
