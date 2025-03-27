0ccd85b44346a7367f1717781462a26ee6e9ca73
Filip Jedrzejewski <fjedrzejewski062@gmail.com>
2025-03-27 20:54:54 +0100

    Poprawienie frontend

---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, email, password, role, user_type, profile_image, banned, deleted, registration_date, last_login)
+VALUES ('prodbyuser', 'user@example.com', '{noop}123456', 'USER', 'BEATMAKER', '/img/default_profile.png', false, false, CURRENT_TIMESTAMP, NULL);
